3d3b4e872e94f82387238f6a08b812a703982d55
lukeloken <lukeloken@gmail.com>
2017-06-30 14:55:59 -0500

    moved meta read to 'Run' Script. Also included warnings for Data

--- R/CutSuperFlameTables.R
+++ R/CutSuperFlameTables.R
@@ -10,18 +10,7 @@
 
 library(data.table)
 
-CutSuperFlame<-function(alldata, dir){
-  
-  #Find meta table
-  rawdir<-paste(dir, 'RawData', sep="/")
-  metafile<-list.files(rawdir)[grep('FlameMeta', list.files(rawdir))]
-  
-  if (length(metafile) != 1) {
-    stop("'dir/RawData' does not contain one FlameMeta file (e.g., 'FlameMetaDate.csv')")}
-  
-  #read meta table and omit rows that are empty
-  meta<-fread(paste(rawdir, metafile, sep="/"), sep=",", skip=0, header=T)
-  meta<-subset(meta, is.na(as.POSIXct(Flame_on, format="%H:%M:%S"))==FALSE)
+CutSuperFlame<-function(alldata, meta){
   
   # Convert on and off times to date_time
   Flame_On <- as.POSIXct(paste(meta$Date[1], meta$Flame_on, sep=" "), format="%Y-%m-%d %H:%M:%S", tz=as.character(meta$GPS_Timezone[1]))
@@ -36,6 +25,8 @@ CutSuperFlame<-function(alldata, dir){
     orderedtimes[aftermidnight:length(orderedtimes)]<-
       as.POSIXct(c(t(data.frame(Flame_On, Flame_Off)+86400)), 
       tz=as.character(meta$GPS_Timezone[1]))[aftermidnight:length(orderedtimes)]
+      
+      warning("Data span more than one day")
   }
   
   #cut data based on flame on/off intervals
@@ -43,8 +34,12 @@ CutSuperFlame<-function(alldata, dir){
   cutdata<-data.frame() 
   for (i in seq(2,length(orderedtimes), by=2)){
     interval=as.POSIXct(orderedtimes[(i-1):i], tz=as.character(meta$GPS_Timezone[1]))
-    cutdata<-rbind(cutdata, subset(alldata,alldata$Date_Time>=interval[1] & alldata$Date_Time<=interval[2]))
+    attr(interval, "tzone") <- "UTC"
+    cutdata<-rbind(cutdata, subset(alldata,alldata$date_time>=interval[1] & alldata$date_time<=interval[2]))
   }
+  
+  if (nrow(cutdata)==0){
+    stop("There are no data within Flame On/Off intervals")}
   
   return(cutdata)
   
