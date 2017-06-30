2bea3902b9a42a7a0ac5c8cc54bfb0b866db7a42
lukeloken <lukeloken@gmail.com>
2017-06-30 15:38:47 -0500

    Added Site and Date to requirements for function

--- R/VisualizeSpatialData.R
+++ R/VisualizeSpatialData.R
@@ -8,7 +8,7 @@
 # ##############################################################
 
 
-PlotSuperFlame<-function(geodata, dir){
+PlotSuperFlame<-function(geodata, dir, Date, Site){
   
   B<-100 #Number of color breaks
   colors<-bpy.colors(n=B, cutoff.tails=0.1, alpha=1)
@@ -22,7 +22,7 @@ PlotSuperFlame<-function(geodata, dir){
         a$Col <- as.numeric(cut(a@data[,i],breaks = B))
         a$Color<-colors[a$Col]
         
-        png(paste(dir, "/Maps/", name,Date, Site, ".png", sep=""), res=200, width=5,height=12, units="in")
+        png(paste(dir, "/Maps/", name, Date, Site, ".png", sep=""), res=200, width=5,height=12, units="in")
         
         par(mar=c(2,1.5,2,0),bg=NA)
         par(mfrow=c(3,1)) 
@@ -50,6 +50,8 @@ PlotSuperFlame<-function(geodata, dir){
         hist(a@data[,i], breaks=25, col='lightgrey', main='', xlab='', ylab='')
         mtext(name, 1, 2)
         mtext('frequency', 2, 2)
+        abline(v=mean(a@data[,i]), col="red", lty=2)
+        abline(v=median(a@data[,i]), col="blue", lty=2)
         
         dev.off()
       }
