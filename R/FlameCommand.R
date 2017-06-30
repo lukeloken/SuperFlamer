922dc5d84712930e5c1a77f6139d9914f65a1d75
lukeloken <lukeloken@gmail.com>
2017-06-30 15:40:13 -0500

    Flame Command Script. This script will be the user interface for running the other scripts.

---
+++ R/FlameCommand.R
@@ -0,0 +1,12 @@
+# ##########################################################################
+# Flame Command
+# Set data diretory and run all flame scripts
+# 1) Data files (.dat) from superflame computer (e.g., Site_Date_GPS.dat)
+# 2) meta tables (.csv) from access (e.g., FlameMetaDate.csv)
+# ##########################################################################
+
+dir<-'E:/Dropbox/FLAME_Light/Data/2017-06-27_LakeMendota'
+
+source('R/RunAllSuperFlameScripts.R')
+
+RunSuperFlame(dir)
