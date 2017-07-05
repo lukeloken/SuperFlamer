bc16b4f2b248e636fa76969f1b335dcd71d3c8f1
lukeloken <lukeloken@gmail.com>
2017-07-05 08:41:16 -0500

    removed lat/long unit conversion

--- R/MergeSuperFlameTables.R
+++ R/MergeSuperFlameTables.R
@@ -47,8 +47,8 @@ ReadSuperFlame<-function(dir){
   #Set Date_Time as time (need to check timezone)
   my.df$date_time<-as.POSIXct(my.df$TIMESTAMP, format='%Y-%m-%d %H:%M:%S', tz='UTC')
   
-  #Set Lat/Long to decimal degrees rather than ddmm.mmmm
-  my.df[,c('longitude', 'latitude')]<-lapply(my.df[,c('longitude', 'latitude')], ConvertToDD)
+  # Set Lat/Long to decimal degrees rather than ddmm.mmmm (likely delete after July 5, 2017)
+  # my.df[,c('longitude', 'latitude')]<-lapply(my.df[,c('longitude', 'latitude')], ConvertToDD)
   
   return(my.df)
 
