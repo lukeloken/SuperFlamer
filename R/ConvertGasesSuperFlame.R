bfedbba80177417c307a73152d7bc9655ab2ceea
lukeloken <lukeloken@gmail.com>
2017-10-10 15:27:24 -0500

    Fixed call for uM. Needed to call as a data frame column rather than a vector

--- R/ConvertGasesSuperFlame.R
+++ R/ConvertGasesSuperFlame.R
@@ -17,26 +17,26 @@ ConvertGasesSuperFlame=function(correctdata, Elevation){
     CH4getsat=as.numeric(getSaturation(CH4kh, Pressure, "CH4"))
     
     convertdata$CH4uM<-as.numeric(convertdata$CH4*CH4kh*Pressure)
-    convertdata$CH4Sat<-as.numeric(CH4uM/CH4getsat*100)
+    convertdata$CH4Sat<-as.numeric(convertdata$CH4uM/CH4getsat*100)
     
     convertdata$CH4uM_hyd<-as.numeric(convertdata$CH4_hyd*CH4kh*Pressure)
-    convertdata$CH4Sat_hyd<-as.numeric(CH4uM_hyd/CH4getsat*100)
+    convertdata$CH4Sat_hyd<-as.numeric(convertdata$CH4uM_hyd/CH4getsat*100)
     
     convertdata$CH4uM_tau<-as.numeric(convertdata$CH4_tau*CH4kh*Pressure)
-    convertdata$CH4Sat_tau<-as.numeric(CH4uM_tau/CH4getsat*100)
+    convertdata$CH4Sat_tau<-as.numeric(convertdata$CH4uM_tau/CH4getsat*100)
     
     #convert CO2 units
     CO2kh=Kh_Plummer(convertdata$temp+273.15)
-    CO2getsat=as.numeric(getSaturation(CO2kh, Pressure, "CO2"))
+    CO2getsat=as.numeric(getSaturation(convertdata$CO2kh, Pressure, "CO2"))
     
     convertdata$CO2uM<-as.numeric(convertdata$CO2*CO2kh*Pressure)
-    convertdata$CO2Sat<-as.numeric(CO2uM/CO2getsat*100)
+    convertdata$CO2Sat<-as.numeric(convertdata$CO2uM/CO2getsat*100)
     
     convertdata$CO2uM_hyd<-as.numeric(convertdata$CO2_hyd*CO2kh*Pressure)
-    convertdata$CO2Sat_hyd<-as.numeric(CO2uM_hyd/CO2getsat*100)
+    convertdata$CO2Sat_hyd<-as.numeric(convertdata$CO2uM_hyd/CO2getsat*100)
     
     convertdata$CO2uM_tau<-as.numeric(convertdata$CO2_tau*CO2kh*Pressure)
-    convertdata$CO2Sat_tau<-as.numeric(CO2uM_tau/CO2getsat*100)
+    convertdata$CO2Sat_tau<-as.numeric(convertdata$CO2uM_tau/CO2getsat*100)
     
   }
   return(convertdata)
