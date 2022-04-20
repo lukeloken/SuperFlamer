660b22da312dc1fce124f4adb559e4f26e9fc25c
Luke Loken <lukeloken@gmail.com>
2022-04-20 08:06:28 -0500

    updated with flamebodia script. Use public table instread of individual scripts

--- R/MergeSuperFlameTables.R
+++ R/MergeSuperFlameTables.R
@@ -7,26 +7,30 @@
 
 library(data.table)
 
-ReadSuperFlame<-function(dir){
-  rawdir<-paste(dir, 'RawData', sep="/")
-  files<-list.files(rawdir)
+ReadSuperFlame <- function(dir){
+  rawdir <- paste(dir, "RawData", sep = "/")
+  files <- list.files(rawdir)
   if (length(files) ==0) {
     stop("No files in 'dir/RawData' subdirectory")}
   
   # Merge data files that contain these strings in the file name
   # Load files in this order
-  patterns<-c('GPS', 'BoxMetrics', 'EXO', 'GGA', 'SUNA')
-  loadfiles<-c(files[grep(patterns[1], files)], 
-                files[grep(patterns[2], files)], 
-                files[grep(patterns[3], files)], 
-                files[grep(patterns[4], files)], 
-                files[grep(patterns[5], files)])
+  # patterns <- c('GPS', 'BoxMetrics', 'EXO', 'GGA', 'SUNA')
+  # loadfiles <- c(files[grep(patterns[1], files)], 
+  #               files[grep(patterns[2], files)], 
+  #               files[grep(patterns[3], files)], 
+  #               files[grep(patterns[4], files)], 
+  #               files[grep(patterns[5], files)])
+  
+  patterns <- c('Public')
+  loadfiles <- c(files[grep(patterns[1], files)])
+                 
   
   if (length(loadfiles) == 0) {
     stop("'dir/RawData' does not contain correct files (e.g., '..._GPS.dat')")}
   
   # Get header names
-  header.list<-lapply(paste(rawdir, loadfiles, sep="/"), fread, sep=",", skip=1, nrows=0)
+  header.list <- lapply(paste(rawdir, loadfiles, sep="/"), fread, sep=",", skip=1, nrows=0)
   # Get data
   import.list <- lapply(paste(rawdir, loadfiles, sep="/"), fread, sep=",", skip=4, header=F, stringsAsFactors=F, na.strings=c('NA', 'NaN', '', 'null', '"NAN"'))
   
@@ -39,13 +43,15 @@ ReadSuperFlame<-function(dir){
   import.list <- lapply(import.list, function(x)x[,-c('RECORD')])
 
   #Merge datatables using TIMESTAMP
-  my.df <- Reduce(function(x, y) merge(x, y, all=FALSE,by=c("TIMESTAMP"),all.x=TRUE, all.y=TRUE),import.list,accumulate=F) 
+  my.df <- Reduce(function(x, y) merge(x, y, all = FALSE, by=c("TIMESTAMP"), 
+                                       all.x = TRUE, all.y = TRUE), 
+                  import.list, accumulate = FALSE) 
   
-  if (nrow(my.df)==0){
+  if (nrow(my.df) == 0){
     stop("Merged data table has zero rows")}
   
   #Set Date_Time as time (need to check timezone)
-  my.df$date_time<-as.POSIXct(my.df$TIMESTAMP, format='%Y-%m-%d %H:%M:%S', tz='UTC')
+  my.df$date_time <- as.POSIXct(my.df$TIMESTAMP, format='%Y-%m-%d %H:%M:%S', tz='UTC')
   
   # Set Lat/Long to decimal degrees rather than ddmm.mmmm (likely delete after July 5, 2017)
   # my.df[,c('longitude', 'latitude')]<-lapply(my.df[,c('longitude', 'latitude')], ConvertToDD)
