11bca4ed6c6573ed8250a5e487a5ca59602e2c93
Luke Loken <lukeloken@gmail.com>
2022-04-20 08:14:49 -0500

    changes after converting to flamebodia

--- R/TrimSuperFlameTables.R
+++ R/TrimSuperFlameTables.R
@@ -8,14 +8,21 @@
 TrimSuperFlame<-function(cutdata){
   
   #Indicate variables to remove
-  gps_vars<-c('date_time', 'latitude', 'longitude', 'speed', 'course', 'depth')
-  box_vars<-c('current_total', 'temp_logger')
-  exo_vars<-c('temp', 'specCond', 'pH', 'pressure', 'chlor_ugL', 'chlor_RFU', 'ODO_percent', 'ODO_mgL', 'BGApc_RFU', 'BGApc_ugL', 'turb_FNU', 'fDOM_RFU', 'fDOM_QSU', 'extPower')
-  gga_vars<-c('CH4', 'H2O', 'CO2')
-  suna_vars<-c('no3_uM', 'nn03_mg', 'abs254', 'abs350', 'temp_int', 'temp_spect', 'temp_lamp')
+  gps_vars <- c('date_time', 'latitude', 'longitude', 
+                'speed', 'course', 'depth')
+  box_vars <- c('temp_logger')
+  exo_vars <- c('temp', 'specCond', 'pH', 'pressure', 
+                'chlor_RFU', 'ODO_percent', 'ODO_mgL', 
+                'BGApc_RFU', 'turb_FNU', 'fDOM_RFU')
+  gga_vars <- c('CH4_Dry', 'H2O', 'CO2_Dry')
+  suna_vars <- c('no3_uM', 'nn03_mg', 
+                 'abs254', 'abs350', 
+                 'temp_int', 'temp_spect', 'temp_lamp')
+  turner_vars <- c("cdom_volt", "peakT_volt")
   
-  vars_good<-c(gps_vars, box_vars, exo_vars, gga_vars, suna_vars)
-  vars_keep<-intersect(vars_good, names(cutdata))
+  
+  vars_good <- c(gps_vars, box_vars, exo_vars, gga_vars, suna_vars, turner_vars)
+  vars_keep <- intersect(vars_good, names(cutdata))
   
   trimdata<-cutdata[,vars_keep, with=FALSE] 
   
