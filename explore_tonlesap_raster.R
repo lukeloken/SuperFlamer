a9cb4c67edfacae0a8643faf05449715196487fd
Luke Loken <lukeloken@gmail.com>
2024-05-15 07:40:57 -0500

    making rasters from tonle sap

--- explore_tonlesap_raster.R
+++ explore_tonlesap_raster.R
@@ -1,42 +1,186 @@
 
+#Explore the tonle sap rasters of NIR imagery. 
+#First data is from January 2022
 
 library(raster)
 library(sf)
 library(sp)
-library(rdgal)
+library(rgdal)
 library(rgeos)
 library(dplyr)
 library(ggplot2)
 library(spdplyr)
+library(qlcMatrix)
 
 #Load wetted perimeter raster
 gis_dir <- "C:/Users/lloken/OneDrive - DOI/GIS"
 
 onedrive_dir <- 'C:/Users/lloken/OneDrive - DOI/FLAMebodia'
 
+#Load the admin boundaries for Tonle Sap and Tonle Chamar
+#load using sf
+TLS_watershed <- st_read(file.path(gis_dir, "Mekong"), "TonleSapWatershed_Polygon")
+TLS_outline <- st_read(file.path(gis_dir, "Mekong"), "TonleSapLakeBoundary")
+TLC_outline <- st_read(file.path(gis_dir, "Mekong"), "TonleChamarPolygon")
 
+TL_rivers <- st_read(file.path(gis_dir, "Mekong"), "TonleSapRiverInputs2") %>%
+  st_combine()
+
+#load the NIR raster
 nir_raster <- raster(file.path(gis_dir, "TonleSap_FromAlan", 
                                "LC08_L1TP_126051_20220102_20220106_02_T1_B5_MERGED.tif"))
 names(nir_raster) <- "layer"
 
 nir_raster <- reclassify(nir_raster, cbind(-Inf, 0.000001, NA), right = FALSE)
 # nir_raster <- clamp(nir_raster, lower = 0.0000001)
+names(nir_raster) <- "layer"
+
+TL_rivers <- st_transform(TL_rivers, crs(nir_raster))
+
+#load the 5 SAR rasters
+sar_files <- list.files(file.path(gis_dir, "Sentinel1", "Oct_2022_VH"), full.names = TRUE)
+sar_list <- lapply(sar_files, raster)
+plot(sar_list[[1]])
+plot(sar_list[[2]])
+
+m1 <- mosaic(sar_list[[1]], sar_list[[2]], fun=max, tolerance = .5)
+m1 <- mosaic(m1, sar_list[[3]], fun=max, tolerance = .5)
+m1 <- mosaic(m1, sar_list[[4]], fun=max, tolerance = .5)
+m1 <- mosaic(m1, sar_list[[5]], fun=max, tolerance = .5)
+
+plot(m1)
+
+saveRDS(m1, 
+        file.path(onedrive_dir, 
+                  "GIS",
+                  "Oct_2022_VH_mosaic.rds"))
+
+
 
 plot(nir_raster)
-res(nir_raster)
+plot(st_geometry(TLS_watershed), add = TRUE)
+plot(st_geometry(TLS_outline), col = "skyblue", add = TRUE)
+plot(st_geometry(TLC_outline), col = "skyblue", add = TRUE)
+plot(st_geometry(TL_rivers), col = "darkblue", add = TRUE)
 
-#aggregate from 30x30 resolution to 300x300 (factor = 10)
-nir_raster_300m <- aggregate(nir_raster, fact = 10)
-res(nir_raster_300m)
-plot(nir_raster_300m)
+#Make a buffer around lakes and subset raster (using 20 km)
+TLS_outline_buffer <- st_buffer(TLS_outline, dist = 1000*20)
+TLC_outline_buffer <- st_buffer(TLC_outline, dist = 1000*20)
+TL_union <- st_union(TLS_outline_buffer, TLC_outline_buffer)
 
-#aggregate from 30x30 resolution to 150x150 (factor = 10)
-nir_raster_150m <- aggregate(nir_raster, fact = 5)
-res(nir_raster_150m)
+plot(st_geometry(TLS_outline_buffer), add = TRUE, fill = NA)
+plot(st_geometry(TLC_outline_buffer), add = TRUE, fill = NA)
+plot(st_geometry(TL_union), add = TRUE, fill = NA, border = "blue", lwd = 2)
+
+
+terra::res(nir_raster)
+
+nir_raster_cropped <- crop(nir_raster, TL_union)
+nir_raster_masked <- mask(nir_raster_cropped, TL_union)
+
+plot(nir_raster_masked)
+# plot(st_geometry(TLS_watershed), add = TRUE)
+plot(st_geometry(TLS_outline), border = "darkblue", add = TRUE, lwd = 2)
+plot(st_geometry(TLC_outline), border = "darkblue", add = TRUE, lwd = 2)
+plot(st_geometry(TL_union), add = TRUE, fill = NA, border = "grey50", lwd = 2)
+plot(st_geometry(TL_rivers), col = "darkblue", add = TRUE)
+
+
+#aggregate from 30x30 resolution to a number of scales
+#Some of these will be used to identify where to predict
+#Courser scales will be used as co-predictors (how much water is around you?)
+
+#aggregate to 150x150 (factor = 5)
+nir_raster_150m <- aggregate(nir_raster_masked, fact = 5)
+terra::res(nir_raster_150m)
 plot(nir_raster_150m)
 
+#aggregate to 300x300 (factor = 10)
+nir_raster_300m <- aggregate(nir_raster_masked, fact = 10)
+terra::res(nir_raster_300m)
+plot(nir_raster_300m)
+
+#aggregate to 600x600m (factor = 20)
+nir_raster_600m <- aggregate(nir_raster_masked, fact = 20)
+terra::res(nir_raster_600m)
+plot(nir_raster_600m)
+
+#aggregate to 1200x1200m (factor = 40)
+nir_raster_1200m <- aggregate(nir_raster_masked, fact = 40)
+terra::res(nir_raster_1200m)
+plot(nir_raster_1200m)
+
+#aggregate to 400x2400m (factor = 80)
+nir_raster_2400m <- aggregate(nir_raster_masked, fact = 80)
+terra::res(nir_raster_2400m)
+plot(nir_raster_2400m)
+
+#aggregate to 4800x4800m (factor = 160)
+nir_raster_4800m <- aggregate(nir_raster_masked, fact = 160)
+terra::res(nir_raster_4800m)
+plot(nir_raster_4800m)
+
+#aggregate to 9600x9600m (factor = 320)
+nir_raster_9600m <- aggregate(nir_raster_masked, fact = 320)
+terra::res(nir_raster_9600m)
+plot(nir_raster_9600m)
+
+#Test summarize neighborhood (300m)
+focal_300m_1500m = focal(nir_raster_300m, w = matrix(1, nrow = 5, ncol = 5), fun = mean)
+plot(focal_300m_1500m)
+
+focal_300m_4500m = focal(nir_raster_300m, w = matrix(1, nrow = 15, ncol = 15), fun = mean)
+plot(focal_300m_4500m)
+
+# focal_300m_1500m_max = focal(nir_raster_300m, w = matrix(1, nrow = 5, ncol = 5), fun = max)
+# plot(focal_300m_1500m_max)
+# 
+# focal_300m_4500m_max = focal(nir_raster_300m, w = matrix(1, nrow = 15, ncol = 15), fun = max)
+# plot(focal_300m_4500m_max)
+
+saveRDS(TL_union, 
+        file.path(onedrive_dir, 
+                  "GIS",
+                  "TL_union.rds"))
 
 
+saveRDS(focal_300m_1500m, 
+        file.path(onedrive_dir, 
+                  "GIS",
+                  "TonleSap_January2022_focal_300m_1500m.rds"))
+
+saveRDS(focal_300m_4500m, 
+        file.path(onedrive_dir, 
+                  "GIS",
+                  "TonleSap_January2022_focal_300m_4500m.rds"))
+
+
+#Test summarize neighborhood (150m)
+focal_150m_1350m = focal(nir_raster_150m, w = matrix(1, nrow = 9, ncol = 9), fun = mean)
+plot(focal_150m_1350m)
+plot(st_geometry(TLS_outline), border = "darkblue", add = TRUE, lwd = 2)
+plot(st_geometry(TLC_outline), border = "darkblue", add = TRUE, lwd = 2)
+
+
+focal_150m_4350m = focal(nir_raster_150m, w = matrix(1, nrow = 29, ncol = 29), fun = mean)
+plot(focal_150m_4350m)
+plot(st_geometry(TLS_outline), border = "darkblue", add = TRUE, lwd = 2)
+plot(st_geometry(TLC_outline), border = "darkblue", add = TRUE, lwd = 2)
+
+
+# focal_150m_1350m_max = focal(nir_raster_150m, w = matrix(1, nrow = 9, ncol = 9), fun = max)
+# plot(focal_150m_1350m_max)
+# plot(st_geometry(TLS_outline), border = "darkblue", add = TRUE, lwd = 2)
+# plot(st_geometry(TLC_outline), border = "darkblue", add = TRUE, lwd = 2)
+# 
+# 
+# focal_150m_4350m_max = focal(nir_raster_150m, w = matrix(1, nrow = 29, ncol = 29), fun = max)
+# plot(focal_150m_4350m_max)
+# plot(st_geometry(TLS_outline), border = "darkblue", add = TRUE, lwd = 2)
+# plot(st_geometry(TLC_outline), border = "darkblue", add = TRUE, lwd = 2)
+
+
+# classify values. group 1 is water
 # create classification matrix
 reclass_df <- c(0, 7360, 1,
                 7360, 13357, 2,
@@ -52,12 +196,63 @@ reclass_m <- matrix(reclass_df,
 
 reclass_m
 
-nir_raster_classified <- reclassify(nir_raster,
+nir_raster_classified <- reclassify(nir_raster_masked,
                                     reclass_m)
-nir_raster_300m_classified <- reclassify(nir_raster_300m,
-                                         reclass_m)
 nir_raster_150m_classified <- reclassify(nir_raster_150m,
                                          reclass_m)
+nir_raster_300m_classified <- reclassify(nir_raster_300m,
+                                         reclass_m)
+nir_raster_600m_classified <- reclassify(nir_raster_600m,
+                                         reclass_m)
+nir_raster_1200m_classified <- reclassify(nir_raster_1200m,
+                                         reclass_m)
+nir_raster_2400m_classified <- reclassify(nir_raster_2400m,
+                                         reclass_m)
+nir_raster_4800m_classified <- reclassify(nir_raster_4800m,
+                                         reclass_m)
+nir_raster_9600m_classified <- reclassify(nir_raster_9600m,
+                                         reclass_m)
+
+#Save each
+saveRDS(nir_raster_classified, 
+        file.path(onedrive_dir, 
+                  "GIS",
+                  "TonleSap_January2022_nir_raster_30m_classified.rds"))
+
+saveRDS(nir_raster_150m_classified, 
+        file.path(onedrive_dir, 
+                  "GIS",
+                  "TonleSap_January2022_nir_raster_150m_classified.rds"))
+
+saveRDS(nir_raster_300m_classified, 
+        file.path(onedrive_dir, 
+                  "GIS",
+                  "TonleSap_January2022_nir_raster_300m_classified.rds"))
+
+saveRDS(nir_raster_600m_classified, 
+        file.path(onedrive_dir, 
+                  "GIS",
+                  "TonleSap_January2022_nir_raster_600m_classified.rds"))
+
+saveRDS(nir_raster_1200m_classified, 
+        file.path(onedrive_dir, 
+                  "GIS",
+                  "TonleSap_January2022_nir_raster_1200m_classified.rds"))
+
+saveRDS(nir_raster_2400m_classified, 
+        file.path(onedrive_dir, 
+                  "GIS",
+                  "TonleSap_January2022_nir_raster_2400m_classified.rds"))
+
+saveRDS(nir_raster_4800m_classified, 
+        file.path(onedrive_dir, 
+                  "GIS",
+                  "TonleSap_January2022_nir_raster_4800m_classified.rds"))
+
+saveRDS(nir_raster_9600m_classified, 
+        file.path(onedrive_dir, 
+                  "GIS",
+                  "TonleSap_January2022_nir_raster_9600m_classified.rds"))
 
 nir_colors <- c("#0077b6", "#c9cba3", "#ffe1a8", "#e26d5c", "#723d46", "#472d30")
 
@@ -65,8 +260,13 @@ nir_colors <- c("#0077b6", "#c9cba3", "#ffe1a8", "#e26d5c", "#723d46", "#472d30"
 
 par(mar= c(2,2,2,2))
 plot(nir_raster_classified, col = nir_colors, axes = FALSE)
-plot(nir_raster_300m_classified, col = nir_colors, axes = FALSE)
 plot(nir_raster_150m_classified, col = nir_colors, axes = FALSE)
+plot(nir_raster_300m_classified, col = nir_colors, axes = FALSE)
+plot(nir_raster_600m_classified, col = nir_colors, axes = FALSE)
+plot(nir_raster_1200m_classified, col = nir_colors, axes = FALSE)
+plot(nir_raster_2400m_classified, col = nir_colors, axes = FALSE)
+plot(nir_raster_4800m_classified, col = nir_colors, axes = FALSE)
+plot(nir_raster_9600m_classified, col = nir_colors, axes = FALSE)
 
 
 
@@ -77,17 +277,126 @@ head(r_df) #breaks will be set to column "layer"
 # r_df$cuts = cut(r_df$layer, breaks=c(0:6)) #set breaks
 
 #preparing raster object to plot with geom_tile in ggplot2
+r_150m_points = rasterToPoints(nir_raster_150m_classified, spatial = TRUE)
+r_150m_df = data.frame(r_150m_points)
+head(r_150m_df) #breaks will be set to column "layer"
+# r_df$cuts = cut(r_df$layer, breaks=c(0:6)) #set breaks
+
+#preparing raster object to plot with geom_tile in ggplot2
 r_300m_points = rasterToPoints(nir_raster_300m_classified, spatial = TRUE)
 r_300m_df = data.frame(r_300m_points)
 head(r_300m_df) #breaks will be set to column "layer"
 # r_df$cuts = cut(r_df$layer, breaks=c(0:6)) #set breaks
 
 #preparing raster object to plot with geom_tile in ggplot2
-r_150m_points = rasterToPoints(nir_raster_150m_classified, spatial = TRUE)
-r_150m_df = data.frame(r_150m_points)
-head(r_150m_df) #breaks will be set to column "layer"
+r_600m_points = rasterToPoints(nir_raster_600m_classified, spatial = TRUE)
+r_600m_df = data.frame(r_600m_points)
+head(r_600m_df) #breaks will be set to column "layer"
 # r_df$cuts = cut(r_df$layer, breaks=c(0:6)) #set breaks
 
+
+
+class_list <- rev(list(
+  #nir_raster_classified, 
+                       # nir_raster_150m_classified
+                       nir_raster_300m_classified
+                       # nir_raster_600m_classified
+                       ))
+
+points_list <- rev(list(
+  # r_points, 
+                        # r_150m_points
+                        r_300m_points
+                        # r_600m_points
+                        ))
+
+res <- rev(c(30, 150, 300, 600))[2]
+res_i <- 1
+for(res_i in 1:length(res)){
+  time_start <- Sys.time()
+
+  points <- points_list[[res_i]]
+  r <- class_list[[res_i]]
+  
+  # create classification matrix
+  reclass_df_2 <- c(0, 1, 1,
+                  1, 2, 2,
+                  2, Inf, 3)
+  reclass_df_2
+  
+  reclass_m_2 <- matrix(reclass_df_2,
+                      ncol = 3,
+                      byrow = TRUE)
+  
+  reclass_m_2
+  
+  r_classified <- reclassify(r,
+                             reclass_m_2)
+  
+  water_points <- filter(points, layer == 1)
+  wet_points <- filter(points, layer == 2)
+  dry_points <- filter(points, layer >= 2)
+  
+  out <- list()
+  for (i in 1:nrow(water_points)) {
+    # for (i in 3871:5000) {
+    d <- distanceFromPoints(r_classified, water_points[i, drop = F])
+    out[[i]] <- zonal(d, r_classified, min)[,2]
+  }
+  a <- do.call(rbind, out)
+  b <- cbind(water_points, a)
+  # names(b)[-1] <- paste("DistTo_", 1:6)
+  
+  b$dist_to_land = apply(b@data[,3:ncol(b@data)], 1, function(x) min(x, na.rm = TRUE))
+  
+  spplot(b, zcol = "dist_to_land", cuts = c(600, seq(800, max(b$dist_to_land), length.out = 10)), main = paste0(res[res_i], "m-resolution"))
+  
+  watergrid_sp <- water_points %>%
+    mutate(dist_to_land = b$dist_to_land, 
+           dist_to_green = b$X2)
+  
+  watergrid_sp$focal_300m_4500m <- raster::extract(focal_300m_4500m, 
+                                                   watergrid_sp, 
+                                                   weights=FALSE, 
+                                                   fun=mean, 
+                                                   na.rm = TRUE)
+  watergrid_sp$focal_300m_1500m <- raster::extract(focal_300m_1500m, 
+                                                   watergrid_sp, 
+                                                   weights=FALSE, 
+                                                   fun=mean, 
+                                                   na.rm = TRUE)
+  
+  watergrid_sf <- st_as_sf(watergrid_sp) 
+  
+  watergrid_sf$dist_to_river = st_distance(watergrid_sf, st_sf(TL_rivers))
+  watergrid_sp$dist_to_river <-  as.numeric(watergrid_sf$dist_to_river)
+  
+  plot(watergrid_sf["dist_to_river"])
+  
+  gridded(watergrid_sp) <- TRUE
+  
+  spplot(watergrid_sp, zcol = "dist_to_land", main = paste0("Distance to land: ", res[res_i], "m-resolution"), pretty = TRUE)
+  spplot(watergrid_sp, zcol = "dist_to_green", main = paste0("Distance to green: ", res[res_i], "m-resolution"), pretty = TRUE)
+  spplot(watergrid_sp, zcol = "dist_to_river", main = paste0("Distance to river: ", res[res_i], "m-resolution"), pretty = TRUE)
+  spplot(watergrid_sp, zcol = "focal_300m_1500m", pretty = TRUE)
+  spplot(watergrid_sp, zcol = "focal_300m_4500m", pretty = TRUE)
+  
+  
+  saveRDS(watergrid_sp, file.path(onedrive_dir, "GIS",
+                                  paste0(                             "TonleSap_January2022_WaterGrid_", res[res_i], "m.rds")))
+  
+  time_end <- Sys.time()
+  message(round(difftime(time_end, time_start, units = "hours"), 2), " hours to process")
+
+  
+}
+
+# dist_to_land <- distanceFromPoints(r, dry_points) 
+
+
+
+
+# spplot(b, zcol = "X2", cuts = c(600, seq(800, max(b$X2), length.out = 10)))
 
 
 # #Raw imagery
@@ -149,7 +458,7 @@ ggsave(file.path(onedrive_dir, "Figures", "Maps_TonleSap", "NIRimagery_Jan2022_1
 
 
 #Make pixel layer to interpolate
-str(r_300m_points)
+# str(r_300m_points)
 
 #using sf package
 # watergrid_300m_sf <- st_as_sf(r_300m_points) %>%
@@ -158,30 +467,39 @@ str(r_300m_points)
 #   geom_sf()
 # watergrid_300m <- as(watergrid_300m_sf, Class = "Spatial") 
 
-
-watergrid_300m_sp <- r_300m_points %>%
-  filter(layer == 1)
-  
-watergrid_150m_sp <- r_150m_points %>%
-  filter(layer == 1)
-
-watergrid_30m_sp <- r_points %>%
-  filter(layer == 1)
-
-
-projection <- proj4string(watergrid_300m_sp)
-
-gridded(watergrid_300m_sp) <- TRUE
-gridded(watergrid_150m_sp) <- TRUE
-
-plot(watergrid_300m_sp, col = "blue")
-plot(watergrid_150m_sp, col = "blue")
-
-
-saveRDS(watergrid_300m_sp, file.path(onedrive_dir, "GIS",
-                                     "TonleSap_January2022_WaterGrid_300m.rds"))
-saveRDS(watergrid_150m_sp, file.path(onedrive_dir, "GIS",
-                                     "TonleSap_January2022_WaterGrid_150m.rds"))
-saveRDS(watergrid_30m_sp, file.path(onedrive_dir, "GIS",
-                                     "TonleSap_January2022_WaterGrid_30m.rds"))
+# watergrid_600m_sp <- r_600m_points %>%
+#   filter(layer == 1) %>%
+#   mutate(dist_to_land = b$dist_to_land, 
+#          dist_to_green = b$X2)
+# 
+# watergrid_300m_sp <- r_300m_points %>%
+#   filter(layer == 1)
+# 
+# watergrid_150m_sp <- r_150m_points %>%
+#   filter(layer == 1)
+# 
+# watergrid_30m_sp <- r_points %>%
+#   filter(layer == 1)
+# 
+# 
+# projection <- proj4string(watergrid_300m_sp)
+# 
+# gridded(watergrid_600m_sp) <- TRUE
+# gridded(watergrid_300m_sp) <- TRUE
+# gridded(watergrid_150m_sp) <- TRUE
+# 
+# plot(watergrid_600m_sp, col = "blue")
+# plot(watergrid_300m_sp, col = "blue")
+# plot(watergrid_150m_sp, col = "blue")
+# 
+# spplot(watergrid_600m_sp, zcol = "dist_to_land")
+# spplot(watergrid_600m_sp, zcol = "dist_to_green")
+# 
+# 
+# saveRDS(watergrid_300m_sp, file.path(onedrive_dir, "GIS",
+#                                      "TonleSap_January2022_WaterGrid_300m.rds"))
+# saveRDS(watergrid_150m_sp, file.path(onedrive_dir, "GIS",
+#                                      "TonleSap_January2022_WaterGrid_150m.rds"))
+# saveRDS(watergrid_30m_sp, file.path(onedrive_dir, "GIS",
+#                                     "TonleSap_January2022_WaterGrid_30m.rds"))
 
